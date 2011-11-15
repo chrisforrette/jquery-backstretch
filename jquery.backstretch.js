817487fd9fe672ac6e1a83f22aabd4c6d5072eb9
Chris Forrette <chris@chrisforrette.com>
2011-11-15 13:00:42 -0800

    Adding 'constrain' option to backstretch

--- jquery.backstretch.js
+++ jquery.backstretch.js
@@ -12,16 +12,18 @@
 (function($) {
 
     $.backstretch = function(src, options, callback) {
+        
         var defaultSettings = {
             centeredX: true,         // Should we center the image on the X axis?
             centeredY: true,         // Should we center the image on the Y axis?
-            speed: 0                 // fadeIn speed for background after image loads (e.g. "fast" or 500)
+            speed: 0,                // fadeIn speed for background after image loads (e.g. "fast" or 500)
+            constrain: false         // If true, the image will not be stretched beyond its original dimensions
         },
         container = $("#backstretch"),
         settings = container.data("settings") || defaultSettings, // If this has been called once before, use the old settings as the default
         existingSettings = container.data('settings'),
         rootElement = ("onorientationchange" in window) ? $(document) : $(window), // hack to acccount for iOS position:fixed shortcomings
-        imgRatio, bgImg, bgWidth, bgHeight, bgOffset, bgCSS;
+        imgRatio, imgWidth, imgHeight, bgImg, bgWidth, bgHeight, bgOffset, bgCSS;
                 
         // Extend the settings with those the user has provided
         if(options && typeof options == "object") $.extend(settings, options);
@@ -49,27 +51,26 @@
                     container.find("img").addClass("deleteable");
                 }
                 
-                img = $("<img />").css({position: "absolute", display: "none", margin: 0, padding: 0, border: "none", zIndex: -999999})
-                                  .bind("load", function(e) {                                          
-                                      var self = $(this),
-                                          imgWidth, imgHeight;
-                                          
-                                      self.css({width: "auto", height: "auto"});
-                                      imgWidth = this.width || $(e.target).width();
-                                      imgHeight = this.height || $(e.target).height();
-                                      imgRatio = imgWidth / imgHeight;
+                img = $("<img />")
+                    .css({position: "absolute", display: "none", margin: 0, padding: 0, border: "none", zIndex: -999999})
+                    .bind("load", function(e) {                                          
+                        var self = $(this);
 
-                                      _adjustBG(function() {
-                                          self.fadeIn(settings.speed, function(){
-                                              // Remove the old images, if necessary.
-                                              container.find('.deleteable').remove();
-                                              // Callback
-                                              if(typeof callback == "function") callback();
-                                          });
-                                      });
-                                      
-                                  })
-                                  .appendTo(container);
+                        self.css({width: "auto", height: "auto"});
+                        imgWidth = this.width || $(e.target).width();
+                        imgHeight = this.height || $(e.target).height();
+                        imgRatio = imgWidth / imgHeight;
+                        
+                        _adjustBG(function() {
+                            self.fadeIn(settings.speed, function(){
+                                // Remove the old images, if necessary.
+                                container.find('.deleteable').remove();
+                                // Callback
+                                if(typeof callback == "function") callback();
+                            });
+                        });
+                    })
+                    .appendTo(container);
                  
                 // Append the container to the body, if it's not already there
                 if($("body #backstretch").length == 0) {
@@ -88,23 +89,27 @@
         function _adjustBG(fn) {
             try {
                 bgCSS = {left: 0, top: 0}
-                bgWidth = rootElement.width();
+                var rootWidth = rootElement.width(),
+                    rootHeight = rootElement.height()
+                bgWidth = (!settings.constrain || rootWidth < imgWidth) ? rootWidth : imgWidth;
                 bgHeight = bgWidth / imgRatio;
                 
-                // Make adjustments based on image ratio
-                // Note: Offset code provided by Peter Baker (http://ptrbkr.com/). Thanks, Peter!
-                if(bgHeight >= rootElement.height()) {
-                    bgOffset = (bgHeight - rootElement.height()) /2;
-                    if(settings.centeredY) $.extend(bgCSS, {top: "-" + bgOffset + "px"});
-                } else {
-                    bgHeight = rootElement.height();
+                if (settings.constrain && bgHeight > imgHeight) {
+                    bgHeight = imgHeight;
                     bgWidth = bgHeight * imgRatio;
-                    bgOffset = (bgWidth - rootElement.width()) / 2;
-                    if(settings.centeredX) $.extend(bgCSS, {left: "-" + bgOffset + "px"});
                 }
+                
+                // Center the image
+                
+                if (settings.centeredY) $.extend(bgCSS, {top: ((rootHeight - bgHeight) / 2) + "px"});
+                if (settings.centeredX) $.extend(bgCSS, {left: ((rootWidth - bgWidth) / 2) + "px"});
 
-                $("#backstretch, #backstretch img").width( bgWidth ).height( bgHeight )
-                                                   .filter("img").css(bgCSS);
+                $("#backstretch, #backstretch img")
+                    .width(bgWidth)
+                    .height(bgHeight)
+                    .filter("#backstretch")
+                    .css(bgCSS);
+                
             } catch(err) {
                 // IE7 seems to trigger _adjustBG before the image is loaded.
                 // This try/catch block is a hack to let it fail gracefully.
